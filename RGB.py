54c1c5f4cf8a6b5c0cfae5c3a1c8011891d5e954
pg3gb <pg3gb@virginia.edu>
2015-12-03 14:58:09 -0500

    Create RGB.py

---
+++ RGB.py
@@ -0,0 +1,64 @@
+# HRI
+# RGB LED for HR-OS1
+
+import RPi.GPIO
+import time
+
+#Establish Pins
+
+GPIO.setmode(GPIO.BCM)
+
+LED_Red = 8
+LED_Green = 9
+LED_Blue = 10
+LED = [LED_Red,LED_Green,LED_Blue]
+
+GPIO.setup(LED_Red, GPIO.OUT)
+GPIO.setup(LED_Green, GPIO.OUT)
+GPIO.setup(LED_Blue, GPIO.OUT)
+
+GPIO.output(LED_Red, 0)
+GPIO.output(LED_Green, 0)
+GPIO.output(LED_Blue, 0)
+
+
+def display(self, color):
+    if (color = 'red'):
+        GPIO.output(LED_Red, true)
+        GPIO.output(LED_Green, 0)
+        GPIO.output(LED_Blue, 0)
+    elif (color = 'green'):
+        GPIO.output(LED_Red, 0)
+        GPIO.output(LED_Green, true)
+        GPIO.output(LED_Blue, 0)
+    elif (color = 'blue'):
+        GPIO.output(LED_Red, 0)
+        GPIO.output(LED_Green, 0)
+        GPIO.output(LED_Blue, true)
+    elif (color = 'yellow'):
+        GPIO.output(LED_Red, true)
+        GPIO.output(LED_Green, true)
+        GPIO.output(LED_Blue, 0)
+    elif (color = 'violet'):
+        GPIO.output(LED_Red, true)
+        GPIO.output(LED_Green, 0)
+        GPIO.output(LED_Blue, true)
+    elif (color = 'teal'):
+        GPIO.output(LED_Red, 0)
+        GPIO.output(LED_Green, true)
+        GPIO.output(LED_Blue, true)
+    elif (color = 'white'):
+        GPIO.output(LED_Red, true)
+        GPIO.output(LED_Green, true)
+        GPIO.output(LED_Blue, true)
+    elif (color = 'off'):
+        GPIO.output(LED_Red, 0)
+        GPIO.output(LED_Green, 0)
+        GPIO.output(LED_Blue, 0)
+        
+        
+        
+
+
+
+    
