5e6a810b437525683e8ff85f008bfb9eb1eb7c1d
pg3gb <pg3gb@virginia.edu>
2015-12-14 15:07:44 -0500

    Update Keyboard.py

--- Keyboard.py
+++ Keyboard.py
@@ -1,54 +1,28 @@
-# HRI
-# RGB LED for HR-OS1
+# HRI - Keyboard Commands for Marley
 #git push
 import RPi.GPIO as GPIO
 import time
 import pygame
+import RGB
 from pygame.locals import *
-#Establish Pins
 
-GPIO.setmode(GPIO.BCM)
+pygame.init()
 
-LED_Red = 2
-LED_Green = 3
-LED_Blue = 4
+done = 0
 
-GPIO.setup(LED_Red, GPIO.OUT)
-GPIO.setup(LED_Green, GPIO.OUT)
-GPIO.setup(LED_Blue, GPIO.OUT)
+while done == False:
+    for event in pygame.event.get():
+        # any other key event input
+        if event.type == pygame.QUIT:
+            done = True
+            
+        elif event.type == KEYDOWN:
+            if event.key == K_ESC:
+                done = 1
 
-GPIO.output(LED_Red, True)
-GPIO.output(LED_Green, True)
-GPIO.output(LED_Blue, True)
+    # get key current state
+    keys = pygame.key.get_pressed()
+    if keys[K_LEFT]:
+        print 'left pressed!'
 
 
-def display(color):
-    GPIO.setwarnings(False)
-    if (color == 'red'):
-        GPIO.output(LED_Red, False)
-        GPIO.output(LED_Green, True)
-        GPIO.output(LED_Blue, True)
-    elif (color == 'green'):
-        GPIO.output(LED_Red, True)
-        GPIO.output(LED_Green, False)
-        GPIO.output(LED_Blue, True)
-    elif (color == 'blue'):
-        GPIO.output(LED_Red, True)
-        GPIO.output(LED_Green, True)
-        GPIO.output(LED_Blue, False)
-    elif (color == 'white'):
-        GPIO.output(LED_Red, False)
-        GPIO.output(LED_Green, False)
-        GPIO.output(LED_Blue, False)
-    elif (color == 'off'):
-        GPIO.output(LED_Red, True)
-        GPIO.output(LED_Green, True)
-        GPIO.output(LED_Blue, True)
-display('off') 
-
-for x in range (0, 10):
-	display('blue')
-	time.sleep(3)
-	display('red')
-	time.sleep(3)
-display('off')
